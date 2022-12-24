a32b2fd4cae375ef19840ee4489f73463a51e74b
Gouttham Chandrasekar <goutthamc@palantir.com>
2022-12-23 19:53:06 -0500

    Remove all other projects from repo

---
+++ create_zip_bash.sh
@@ -0,0 +1,4 @@
+cd /Users/goutthamc/personal-repos/timetracker/timetracker_lambda/src
+zip -r /Users/goutthamc/personal-repos/timetracker/my-deployment-package.zip .
+cd /Users/goutthamc/personal-repos/timetracker/timetracker_lambda/lib/python3.9/site-packages
+zip -r /Users/goutthamc/personal-repos/timetracker/my-deployment-package.zip .
--- timetracker_lambda/src/lambda_function.py
+++
@@ -1,8 +0,0 @@
-import json
-from copy_gcal_to_timelogger import gc_to_tl_sync_history_for_last_x_days
-from copy_timelogger_to_gcal import tl_to_gc_sync_history_for_last_x_days
-
-def lambda_handler(event, context):
-    gc_to_tl_sync_history_for_last_x_days(2)
-    tl_to_gc_sync_history_for_last_x_days(2)
-
