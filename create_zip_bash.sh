1ffb2ded4332e5919baa010b70ed166e78ee0ed9
Gouttham Chandrasekar <goutthamc@palantir.com>
2022-12-23 21:17:27 -0500

    Use relative imports in .sh file

--- create_zip_bash.sh
+++ create_zip_bash.sh
@@ -1,4 +1,2 @@
-cd /Users/goutthamc/personal-repos/timetracker/timetracker_lambda/src
-zip -r /Users/goutthamc/personal-repos/timetracker/my-deployment-package.zip .
-cd /Users/goutthamc/personal-repos/timetracker/timetracker_lambda/lib/python3.9/site-packages
-zip -r /Users/goutthamc/personal-repos/timetracker/my-deployment-package.zip .
+zip -r my-deployment-package.zip ./src
+zip -r my-deployment-package.zip ./lib
