6896e75395a8a6962158031507bcd5a5c8013dfe
Gouttham Chandrasekar <goutthamc@palantir.com>
2022-12-23 20:00:08 -0500

    test change

---
+++ timetracker_lambda/src/lambda_function.py
@@ -0,0 +1,12 @@
+import datetime
+from copy_gcal_to_timelogger import sync_gc_entries_to_timelogger
+from copy_timelogger_to_gcal import tl_to_gc_sync_history_for_last_x_days
+
+def lambda_handler(event, context):
+
+    from_endtime =  datetime.now() - datetime.timedelta(days=2)
+    to_endtime = datetime.now() - datetime.timedelta(hours=4)
+    sync_gc_entries_to_timelogger(from_endtime, to_endtime)
+
+    tl_to_gc_sync_history_for_last_x_days(2)
+
